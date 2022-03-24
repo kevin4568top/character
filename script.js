f8b57450ff9e5050c5ba314c8d526dff05da1f2e
kevin4568top <kevin4568topp@gmail.com>
2022-03-24 20:42:00 +0800

    v1.1

---
+++ script.js
@@ -0,0 +1,12 @@
+/*window.onload = function(){
+    setInterval("toggleSound()",100);
+}
+
+function toggleSound() {
+       var music = document.getElementById("aud");//獲取ID
+
+       if (music.paused) { //判讀是否播放
+           music.paused=false;
+           music.play(); //沒有就播放
+       }
+}*/
